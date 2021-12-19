06222fee09b4d60a27d02164da92ba8c190b27e9
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2021-12-19 00:21:56 -0300

    random shuffle

--- cripta/src/App.js
+++ cripta/src/App.js
@@ -4,14 +4,30 @@ import Tablero from './Components/Tablero'
 import Operation from './Components/Operation'
 import Info from './Components/Info'
 
+
 function App() {
-  const [cards, setCards] = useState([
-    { valor: 1, palo: "espada" },
-    { valor: 4, palo: "espada" },
-    { valor: 6, palo: "basto" },
-    { valor: 12, palo: "oro" },
-    { valor: 8, palo: "copa" },
-  ]);
+  function checkIfDuplicateExists(arr) {
+    return new Set(arr).size !== arr.length
+  }
+
+  const randomCards = (n) => {
+    let types = ["basto", "espada", "copa", "oro"];
+    let cards = [];
+
+    for (let i = 0; i < n; i++) {
+      cards.push({ valor: Math.ceil(Math.random() * 11), palo: types[Math.floor(Math.random() * 4)] })
+    }
+
+    let codes = cards.map(carta => carta.valor + carta.palo);
+
+    if (checkIfDuplicateExists(codes)) {
+      return randomCards(n)
+    }
+    return cards;
+  }
+
+  const [cards, setCards] = useState(randomCards(5));
+
 
   const [pressed, setPressed] = useState([false, false, false, false]);
   const [calculation, setCalculation] = useState("");
@@ -23,9 +39,8 @@ function App() {
       temp[idx] = true;
       setPressed(temp);
       setLastType("num");
-      setCalculation(calculation + cards[idx + 1].valor + " ");
+      setCalculation(calculation + cards[idx].valor + " ");
     }
-    console.log("STATE", pressed)
   }
   const updateCalculation = (val) => {
     if (val == "(" && lastType != "num") {
@@ -60,6 +75,7 @@ function App() {
           setCalculation("");
           setPressed([false, false, false, false]);
           setLastType("result");
+          setCards(randomCards(5));
         }, 2000);
 
       } catch (error) {
@@ -97,20 +113,17 @@ function App() {
           <Card valor={cards[cards.length - 1].valor} palo={cards[cards.length - 1].palo} />
         }
       >
-        {cards.slice(1, cards.length).map((card, idx) => {
-          return <>
-            <div className={`card ${!pressed[idx] ? 'cardPressable' : 'cardNotPressable'}`} onClick={() => pressCard(idx)} key={card.valor + " " + card.palo} >
-              <Card valor={card.valor} palo={card.palo} />
-            </div>
-          </>
+        {cards.map((card, idx) => {
+          if (idx < 4) {
+            return (
+              <div className={`card ${!pressed[idx] ? 'cardPressable' : 'cardNotPressable'}`} onClick={() => pressCard(idx)} key={card.valor + " " + card.palo} >
+                <Card valor={card.valor} palo={card.palo} />
+              </div>
+            )
+          }
         })}
 
       </Tablero>
-      {/* <div className='operationBoard'>
-      
-
-    </div> */}
-
       <div className="bottomContainer">
         <p className="calculation">{calculation ? calculation : `Target: ${cards[cards.length - 1].valor}`}</p>
       </div>
